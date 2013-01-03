af436c8a64fa29cdaf3beae061101a768b0b939d
David Gossow <dgossow@willowgarage.com>
2013-01-02 17:20:49 -0800

    fixed dependencies etc in package.xml/CMakelists

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Willow Garage, Inc.
+ * Copyright (c) 2012, Willow Garage, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
